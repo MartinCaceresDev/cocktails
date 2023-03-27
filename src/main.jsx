0af3872c7f7d26932bd9cc3684ba754b0d516edc
Martin Caceres <martincaceres@outlook.com>
2023-03-27 13:34:16 -0300

    sass implementation

--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,9 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import { BrowserRouter, Route, Routes } from 'react-router-dom'
-import App from './App'
-import { AppProvider } from './context'
-import './index.css'
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import App from './App';
+import { AppProvider } from './context';
+import './scss/index.scss';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
