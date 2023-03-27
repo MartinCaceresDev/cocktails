09de06118c24c6570b898754583b68e4d438ec80
Martin Caceres <martincaceres@outlook.com>
2023-03-27 13:45:43 -0300

    sass implemented

--- src/main.jsx
+++ src/main.jsx
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom/client';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import App from './App';
 import { AppProvider } from './context';
-import './scss/index.scss';
+import './scss/main.scss';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
